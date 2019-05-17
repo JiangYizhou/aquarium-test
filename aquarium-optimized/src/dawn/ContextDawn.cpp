6754df221d2183eb51be0b2dbab0a94501a625a3
Yizhou Jiang <yizhou.jiang@intel.com>
2019-05-17 15:25:03 +0800

    Revise msaa resolve format to preferredSwapChainFormat on dawn backend

--- aquarium-optimized/src/dawn/ContextDawn.cpp
+++ aquarium-optimized/src/dawn/ContextDawn.cpp
@@ -179,7 +179,7 @@ bool ContextDawn::createContext(BACKENDTYPE backend, bool enableMSAA)
         descriptor.size.depth      = 1;
         descriptor.arrayLayerCount = 1;
         descriptor.sampleCount     = 4;
-        descriptor.format          = dawn::TextureFormat::R8G8B8A8Unorm;
+        descriptor.format          = mPreferredSwapChainFormat;
         descriptor.mipLevelCount   = 1;
         descriptor.usage           = dawn::TextureUsageBit::OutputAttachment;
 
